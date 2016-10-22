f61f34ba5a580b1c5dec10646d766109b6f939ba
Jakub Knetl <jknetl@redhat.com>
2016-10-22 11:54:27 +0200

    Display additional components in the editor

--- pc4ide-editor/src/main/java/org/perfcake/ide/editor/controller/impl/EditorController.java
+++ pc4ide-editor/src/main/java/org/perfcake/ide/editor/controller/impl/EditorController.java
@@ -2,9 +2,13 @@ package org.perfcake.ide.editor.controller.impl;
 
 import org.perfcake.ide.core.model.AbstractModel;
 import org.perfcake.ide.core.model.GeneratorModel;
+import org.perfcake.ide.core.model.MessagesModel;
+import org.perfcake.ide.core.model.ReceiverModel;
 import org.perfcake.ide.core.model.ReportingModel;
 import org.perfcake.ide.core.model.ScenarioModel;
 import org.perfcake.ide.core.model.SenderModel;
+import org.perfcake.ide.core.model.SequencesModel;
+import org.perfcake.ide.core.model.ValidationModel;
 import org.perfcake.ide.editor.controller.AbstractController;
 import org.perfcake.ide.editor.controller.Controller;
 import org.perfcake.ide.editor.controller.RootController;
@@ -14,8 +18,12 @@ import org.perfcake.ide.editor.forms.FormManager;
 import org.perfcake.ide.editor.view.ComponentView;
 import org.perfcake.ide.editor.view.UnsupportedChildViewException;
 import org.perfcake.ide.editor.view.icons.GeneratorIcon;
+import org.perfcake.ide.editor.view.icons.MessageIcon;
+import org.perfcake.ide.editor.view.icons.ReceiverIcon;
 import org.perfcake.ide.editor.view.icons.ReporterIcon;
 import org.perfcake.ide.editor.view.icons.SenderIcon;
+import org.perfcake.ide.editor.view.icons.SequenceIcon;
+import org.perfcake.ide.editor.view.icons.ValidatorIcon;
 import org.perfcake.ide.editor.view.impl.EditorView;
 
 import javax.swing.JComponent;
@@ -48,6 +56,18 @@ public class EditorController extends AbstractController implements RootControll
 			} else if (child instanceof ReportingModel) {
 				final Controller reporting = new SectionController("Reporting", new ReporterIcon(), child);
 				addChild(reporting);
+			} else if (child instanceof SequencesModel){
+				final Controller sequences = new SectionController("Sequences", new SequenceIcon(), child);
+				addChild(sequences);
+			} else if (child instanceof ReceiverModel){
+				final Controller receiver = new SectionController("Receiver", new ReceiverIcon(), child);
+				addChild(receiver);
+			} else if (child instanceof MessagesModel){
+				final Controller messages = new SectionController("MessagesModel", new MessageIcon(), child);
+				addChild(messages);
+			} else if (child instanceof ValidationModel){
+				final Controller validation = new SectionController("ValidationModel", new ValidatorIcon(), child);
+				addChild(validation);
 			}
 		}
 	}
